cac0386bfa7ef52ee6422d041d7264ec15a00040
MattressAIBlake <blake@themattressai.com>
2024-10-09 22:18:01 -0500

    2nd try debugging

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['picsum.photos'],
   },
-}
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
